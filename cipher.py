bd2e14c118793f47e54079ef124041beb30cabd5
aiangelov127 <aiangelov127@gmail.com>
2021-10-21 06:40:05 +0300

    Cipher to encode messages
    Put in your message and your key and you will get an encrypted message that you can not read without the key.

--- cipher.py
+++ cipher.py
@@ -1,52 +1,37 @@
-from os import closerange
-import string
-
 alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', '1', 'h', 'i', 'j', 'k', 'l', 'm', '2', 'n', 'o', 'p', 'q', 'r', 's', 't', '3', 'u', 'v', 'w', '4', 'x', 'y', 'z', '5', ' ', '6', '!', '@', '#', '$', '%', '^', '&', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '7', 'O', 'P', 'Q', 'R', 'S', '8', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*', '9', '(', ')', ',', '0', '.', '/', '?']
 
-print("Welcome. You can encrypt or decrypt a message here. ")
-user_choice = input("Encode or decode a message? ")
-input_text = input("Write your message. ")
-text = list(input_text)
-cipher_value = int(input("Choose a cipher index. ")) #int(input("Choose an index: "))
-#_________________________________________________________Defines what happens if cypher is larger than alphabet_list
-def text_char_index(text):
-    char_index_list = []
-    for char in text:
-        char_index = alphabet_list.index(char)
-        char_index_list.append(char_index)
-    return char_index_list
-text_char_index = text_char_index(text)
-#__________________________________________________Turns characters into numbers according to the alphabet_list
-def in_numbers(user_choice, cipher_value): 
-    encoded_list = []
-    for char in text_char_index:
-        if user_choice == "encode":
-            if int(char) + cipher_value > len(alphabet_list) - 1:
-                encoded_char = int(char) + cipher_value - (len(alphabet_list))
-            else:
-                encoded_char = int(char) + cipher_value
-        elif user_choice == "decode":
-            if int(char) + cipher_value > len(alphabet_list) - 1: 
-                encoded_char = int(char) + cipher_value - (len(alphabet_list))
-            else:
-                encoded_char = int(char) - cipher_value
-        else:
-            print("Choose a correct operation. ")
-        encoded_list.append(encoded_char)
-    return encoded_list
-in_numbers = in_numbers(user_choice, cipher_value)
-#____________________________________________________________________Adds or removes cipher_value
-def in_characters(in_numbers, alphabet_list):
-    encoded_list = []
-    for encoded_num in in_numbers:
-        encoded_char = alphabet_list[encoded_num]
-        encoded_list.append(encoded_char)
-    return encoded_list
-in_characters = in_characters(in_numbers, alphabet_list)
-#__________________________________________________________Turns numbers into characters according to alphabet_list.
-def message_to_string(in_characters):
-    text_string = ''.join(in_characters)
-    return text_string
-text_string = message_to_string(in_characters)
+user_choice = input("What do you want to do? Encrypt or decrypt?")
+text = input("Write your message. \n")
+cipher_index = int(input("Chose a cipher key: \n"))
 
-print(f"The message is \n {text_string}")
+def encrypt(text, cipher_index):
+    encrypted=""
+    for letter in text:
+        position = alphabet_list.index(letter)
+        if position + cipher_index > len(alphabet_list)-1:
+            new_position = position + cipher_index - len(alphabet_list)
+        else:
+            new_position = position + cipher_index
+        new_letter = alphabet_list[new_position]
+        encrypted += new_letter
+    print(encrypted)
+
+
+def decrypt(text, cipher_index):
+    decrypted=""
+    for letter in text:
+        position = alphabet_list.index(letter)
+        if position + cipher_index > len(alphabet_list)-1:
+            new_position = position + cipher_index - len(alphabet_list)
+        else:
+            new_position = position - cipher_index
+        new_letter = alphabet_list[new_position]
+        decrypted += new_letter
+    print(decrypted)
+
+if user_choice == "encrypt":
+    encrypt(text, cipher_index)
+elif user_choice == "decrypt":
+    decrypt(text, cipher_index)
+else:
+    print("Please chose a correct option.")
